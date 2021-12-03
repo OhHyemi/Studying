e6706439481e9298b9851277e653dac0dfff60ba
OhHyemi <gpal1014@naver.com>
2021-12-03 14:43:26 +0900

    studying interitance

---
+++ CSharpStudying/CSharpStudying/Monster.cs
@@ -0,0 +1,73 @@
+﻿namespace CSharpStudying
+{
+    public abstract class Monster
+    {
+        private int maxHp;
+        private int maxMp;
+        private int hp;
+        private int mp;
+
+        private bool isAbleSkill;
+        public bool IsAbleSkill
+        {
+            get => mp > 0;
+            private set => isAbleSkill = value;
+        }
+
+        private bool IsAlive=> hp > 0;
+        public int Hp
+        {
+            get => hp;
+            set
+            {
+                var _hp = hp + value;
+                if (_hp > maxHp)
+                {
+                    hp = maxHp;
+                }
+                else if (_hp <= 0)
+                {
+                    Die();
+                }
+                else
+                {
+                    hp = _hp;
+                }
+            }
+        }
+        public int Mp
+        {
+            get => mp;
+            set
+            {
+                var _mp = mp + value;
+                if (_mp > maxMp)
+                {
+                    mp = maxMp;
+                }
+                else if (_mp <= 0)
+                {
+                    mp = 0;
+                }
+                else
+                {
+                    mp = _mp;
+                }
+            }
+        }
+
+        protected Monster(int maxHp, int maxMp)
+        {
+            this.maxHp = maxHp;
+            this.maxMp = maxMp;
+            hp = maxHp;
+            mp = maxMp;
+        }
+        public abstract void UseSkill();
+        public abstract void Die();
+        public void SetEnableUseSkill(bool able)
+        {
+            isAbleSkill = able;
+        }
+    }
+}
---
+++ CSharpStudying/CSharpStudying/Orc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace CSharpStudying
+{
+    public class Orc : Monster
+    {
+        public override void UseSkill()
+        {
+            if (IsAbleSkill)
+            {
+                Skill();
+            }
+        }
+        public Orc(int maxHp, int maxMp) : base(maxHp,maxMp)
+        {
+        }
+        protected virtual void Skill()
+        {
+            Console.WriteLine("일반오크 스킬 발동!");
+        }
+        public override void Die()
+        {
+            Console.WriteLine("오크 죽음!");
+        }
+    }
+    public class WizardOrc : Orc, IFly
+    {
+        public WizardOrc(int maxHp, int maxMp) : base(maxHp,maxMp)
+        {
+        }
+        protected override void Skill()
+        {
+            Console.WriteLine("마법사 오크 스킬 발동!");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("휘익~ 마법사 오크 난다!");
+        }
+    }
+    public class KnightOrc : Orc
+    {
+        public KnightOrc(int maxHp, int maxMp) : base(maxHp,maxMp)
+        {
+        }
+        protected override void Skill()
+        {
+            Console.WriteLine("전사오크 스킬 발동!");
+        }
+    }
+    public class ArcherOrc : Orc
+    {
+        public ArcherOrc(int maxHp, int maxMp) : base(maxHp,maxMp)
+        {
+        }
+        protected override void Skill()
+        {
+            Console.WriteLine("궁수오크 스킬 발동!");
+        }
+    }
+}
